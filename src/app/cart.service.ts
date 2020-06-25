55e0af190ffd461fd6f7d63eea125aa58940e19f
Juan Manuel <jnmnlg@gmail.com>
2020-06-25 18:46:45 -0300

    Fin desa

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CartService {
-  items =[];
+  items = [];
   addToCart(product){
     this.items.push(product);
   }
